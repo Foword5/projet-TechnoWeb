63bc568e8c400cd1f22cb5b96360c0b9738b4490
Lelievre-david <81243462+Lelievre-david@users.noreply.github.com>
2023-02-26 10:55:18 -0500

    1ere route pas tout a fait finis mais fonctionnel

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from playhouse.shortcuts import model_to_dict, dict_to_model
 import urllib.request
 import json
@@ -12,6 +12,69 @@ def products():
     for product in Product.select():
         products["products"].append(model_to_dict(product))
     return jsonify(products)
+
+#create new order
+@app.route('/order', methods=['POST'])
+def create_order():
+
+    #check if the body contains a product with an id and a quantity
+    if "product" not in request.json or "id" not in request.json["product"] or "quantity" not in request.json["product"]:
+        return jsonify(
+            { "errors" : 
+             { "product": 
+              { "code": "missing-fields", "name": "La création d'une commande nécessite un produit" } 
+                } 
+            } ), 422
+    #check if the quantity is superior or equal to 1
+    if request.json["product"]["quantity"] < 1:
+        return jsonify(
+            { "errors" : 
+             { "product": 
+              { "code": "missing-fields", "name": "La quantité doit être supérieure ou égale à 1" } 
+                } 
+            } ), 422
+    
+    #get the product if it exists
+    try:
+        product = Product.get(Product.id == request.json["product"]["id"])
+    except Product.DoesNotExist:
+        return jsonify(
+            { "errors" : 
+             { "product": 
+              { "code": "not-found", "name": "Le produit demandé n'existe pas" } 
+                } 
+            } ), 422
+    if product.in_stock == False :
+        return jsonify(
+            { "errors" : 
+             { "product": 
+              { "code": "out-of-inventory", "name": "Le produit demandé n'est pas en inventaire" } 
+                } 
+            } ), 422
+    
+    quantity = request.json["product"]["quantity"]
+    #calculate the shipping price
+    total_price = product.price * quantity
+    
+    #calculate the shipping price based on the weight of the product
+    if product.weight * quantity < 500:
+        shipping_price = total_price + 5
+    elif product.weight * quantity < 2000:
+        shipping_price = total_price + 10
+    else:
+        shipping_price = total_price + 25
+
+    #create the order
+    order = Order.create(
+        total_price = total_price,
+        shipping_price = shipping_price,
+        product = product,
+        quantity = quantity
+    )
+    return jsonify(model_to_dict(order))
+
+
+
 
 @app.cli.command("init-db") # s'exécute avec la commande flask init-db
 def init_db():
